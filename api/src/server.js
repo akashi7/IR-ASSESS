6cf8bf505dafbbc3a44f3c1f4df36ab87ee7f14b
akashi7 <christiannseko@gmail.com>
2026-02-10 15:52:12 +0200

    api url

--- api/src/server.js
+++ api/src/server.js
@@ -61,7 +61,7 @@ const startServer = async () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error('Unable to connect to the database ui:', error);
     process.exit(1);
   }
 };
